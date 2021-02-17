819530eff29583c60cd42ac70f2fa441b3dc2872
Vinícius Reis <vinicius.moreira2003@gmail.com>
2021-02-17 18:07:10 -0300

    Criando Tabelas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function homepage() {
+        $variavel = "Homepage do sistema de gestão para o grupo de investismentos";
+
+        return view('welcome', [
+            'title' => $variavel
+        ]);
+    }
+
+    public function cadastrar() {
+        echo "Tela de cadastro";
+    }
+
+    public function login() {
+        echo "Tela de login";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [Controller::class, 'homepage']);
+Route::get('/cadastro', [Controller::class, 'cadastrar']);
+Route::get('/login', [Controller::class, 'login']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
